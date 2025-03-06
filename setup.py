0f07855e70f55029d38f4ae8312b40e233c21b85
Eric Spencer <espencer2@luc.edu>
2025-03-06 13:28:18 -0600

    path executable

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ Usage: python setup.py py2app
 """
 from setuptools import setup
 
-APP = ['app.py']  # replace with your filename
+APP = ['main.py']  # replace with your filename
 DATA_FILES = []
 OPTIONS = {
-    # 'alias': True,              # uses symlinks instead of copying resources
     'iconfile': 'icon.icns',    # use a proper .icns file for the macOS app icon
 }
 
